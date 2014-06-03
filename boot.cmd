fe55d1d95b1d0f0e16c332b57d7e93987c0e77ab
David Lechner <david@lechnology.com>
2014-06-03 16:07:55 -0500

    update boot.cmd to match flash-kernel package

--- boot.cmd
+++ boot.cmd
@@ -1,11 +1,5 @@
 run loadimage
-
-# boot from SD card
-setenv rootfs /dev/mmcblk0p2
-
-# boot from nfs (change nfsroot=... to match your installation)
-#setenv rootfs /dev/nfs nfsroot=192.168.0.1:/path/to/rootfs,nolock,rsize=1024
-
-setenv bootargs mem=${memsize} console=${console} root=${rootfs} rw rootwait ip=${ipaddr} lpj=747520
-run mmcboot
+fatload mmc 0 ${filesysaddr} uInitrd
+setenv bootargs mem=${memsize} console=${console} initrd=${filesysaddr},${filesyssize} root=/dev/ram rw rootwait lpj=747520 splash fbcon=font:ProFont6x11 plymouth.ignore-serial-consoles break=premount
+bootm ${loadaddr} ${filesysaddr}
 
